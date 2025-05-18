bc695adccd80693996d256f737a5ae4494b141f0
Hasan Akgun <akgunhasan8@gmail.com>
2025-05-19 00:39:38 +0300

    Setup react

---
+++ education-platform-frontend/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.css'
+
+export function Header() {
+  return (
+    <div className="header">
+      <h1>Welcome to the Education Platform</h1>
+      <p>Your one-stop solution for all educational resources.</p>
+    </div>
+  )
+}
---
+++ education-platform-frontend/src/pages/home/HomePage.jsx
@@ -0,0 +1,9 @@
+import './HomePage.css';
+import { Header } from '../../components/Header';
+export function HomePage() {
+  return (
+    <div className="home-page">
+      <Header />
+    </div>
+  );
+}
